5417c8415cfc0521ed06c755254567297d76aa02
Bosco CHEUNG <tl.cheung1@ha.org.hk>
2026-06-29 11:34:04 +0800

    chore: update package version to 1.4.1 and adjust styles in IntroductionModalComponent

--- bosco-mini-portfolio/src/components/modal/introduction/IntroductionModalComponent.tsx
+++ bosco-mini-portfolio/src/components/modal/introduction/IntroductionModalComponent.tsx
@@ -6,8 +6,8 @@ import { translationKeys } from "../../../globalVariable/Translation"
 
 export default function IntroductionModalComponent() {
   // style variable
-  const headlineStyle = 'text-center font-medium text-[#0B1A33] dark:text-white text-[20px] sm:text-[20px] md:text-[20px] lg:text-[25px]'
-  const textStyle = 'mt-[0.5rem] text-justify text-[14px] sm:text-[14px] md:text-[14px] lg:text-[16px] text-[#9A9A9A] dark:text-[#94A3B8]'
+  const headlineStyle = 'text-center font-medium text-[#334155] dark:text-white text-[20px] sm:text-[20px] md:text-[20px] lg:text-[25px]'
+  const textStyle = 'mt-[0.5rem] text-justify text-[14px] sm:text-[14px] md:text-[14px] lg:text-[16px] text-[#334155] dark:text-[#94A3B8]'
   // translation
   const { t } = useContext(MapperContext)
 
